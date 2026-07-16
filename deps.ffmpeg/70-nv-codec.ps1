51b514463d1f8b71196e9d33b5e75a6fa1de17d5
Ryan Foster <ryan@obsproject.com>
2026-07-16 14:24:09 -0400

    deps.ffmpeg: Patch nv-codec-headers to enable Windows on Arm

--- deps.ffmpeg/70-nv-codec.ps1
+++ deps.ffmpeg/70-nv-codec.ps1
@@ -3,11 +3,27 @@ param(
     [string] $Version = '13.0.19.0',
     [string] $Uri = 'https://github.com/FFmpeg/nv-codec-headers.git',
     [string] $Hash = 'e844e5b26f46bb77479f063029595293aa8f812d',
-    [array] $Targets = @('x64')
+    [array] $Targets = @('x64'),
+    [array] $Patches = @(
+        @{
+            PatchFile = "${PSScriptRoot}/patches/nv-codec/0001-Load-nvEncodeAPI.dll-for-native-Windows-ARM64-Windows.patch"
+            HashSum = "C8CB308261D84F3D95DAD56EF3813677D6B4EFBE2325E53F98EF2CF96B61E5B3"
+        }
+    )
 )
 
 function Setup {
     Setup-Dependency -Uri $Uri -Hash $Hash -DestinationPath $Path
+}
+
+function Patch {
+    Log-Information "Patch (${Target})"
+    Set-Location $Path
+
+    $Patches | ForEach-Object {
+        $Params = $_
+        Safe-Patch @Params
+    }
 }
 
 function Build {
--- deps.ffmpeg/70-nv-codec.zsh
+++ deps.ffmpeg/70-nv-codec.zsh
@@ -5,6 +5,10 @@ local name='nv-codec-headers'
 local version='13.0.19.0'
 local url='https://github.com/ffmpeg/nv-codec-headers.git'
 local hash='e844e5b26f46bb77479f063029595293aa8f812d'
+local -a patches=(
+  "* ${0:a:h}/patches/nv-codec/0001-Load-nvEncodeAPI.dll-for-native-Windows-ARM64.patch \
+    86eecb205665903c13aa1c55e49cecfee45aa8482e422616905bcc0d7a0d37a3"
+)
 
 ## Dependency Overrides
 local targets=('windows-x*')
@@ -13,6 +17,25 @@ local targets=('windows-x*')
 setup() {
   log_info "Setup (%F{3}${target}%f)"
   setup_dep ${url} ${hash}
+}
+
+patch() {
+  autoload -Uz apply_patch
+
+  log_info "Patch (%F{3}${target}%f)"
+
+  cd ${dir}
+
+  local patch
+  local _target
+  local _url
+  local _hash
+
+  for patch (${patches}) {
+    read _target _url _hash <<< "${patch}"
+
+    if [[ ${target%%-*} == ${~_target} ]] apply_patch ${_url} ${_hash}
+  }
 }
 
 build() {
